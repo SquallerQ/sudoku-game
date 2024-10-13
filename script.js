ae511b014174d5426f6ceaa5f40e6a2aca98f21e
squallerq <john199502031@gmail.com>
2024-10-13 16:48:29 +0200

    feat: add menu and score table

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// const Row1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// const Row2 = [4, 5, 6, 7, 8, 9, 1, 2, 3];
-// const Row3 = [7, 8, 9, 1, 2, 3, 4, 5, 6];
-// const Row4 = [2, 3, 4, 5, 6, 7, 8, 9, 1];
-// const Row5 = [5, 6, 7, 8, 9, 1, 2, 3, 4];
-// const Row6 = [8, 9, 1, 2, 3, 4, 5, 6, 7];
-// const Row7 = [3, 4, 5, 6, 7, 8, 9, 1, 2];
-// const Row8 = [6, 7, 8, 9, 1, 2, 3, 4, 5];
-// const Row9 = [9, 1, 2, 3, 4, 5, 6, 7, 8];
-
 const basicMatrix = [
   [1, 2, 3, 4, 5, 6, 7, 8, 9],
   [4, 5, 6, 7, 8, 9, 1, 2, 3],
@@ -19,12 +9,19 @@ const basicMatrix = [
   [6, 7, 8, 9, 1, 2, 3, 4, 5],
   [9, 1, 2, 3, 4, 5, 6, 7, 8]
 ];
+
 let fullGridWithAllCeils = []
 let gridWithEmptyCeils = []
+
+let score = 0;
+let moveCount = 0;
 
 const sudokuGrid = document.querySelector('.game__inner')
 const sudokuRows = document.querySelectorAll('.game__inner-row')
 const sudokuBox = document.querySelectorAll('.game__inner-box')
+
+const scoreOnPage = document.querySelector('.game__menu-score span');
+const movesOnPage = document.querySelector('.game__menu-move span');
 
 
 function renderGrid () {
@@ -68,8 +65,7 @@ disabledBoxes(35)
 
 
 
-console.log(fullGridWithAllCeils);
-console.log(gridWithEmptyCeils);
+
 
 function getRandomNumbers(max) {
   const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -110,18 +106,20 @@ sudokuGrid.addEventListener("click", function (event) {
 });
 
 sudokuGrid.addEventListener("keydown", function (event) {
-  if (event.keyCode > 48 && event.keyCode < 58) {
-    event.target.value = event.key;
-  } else if (event.keyCode === 8 || event.keyCode === 46) {
+  let userAnswer = '';
+  if (event.keyCode === 8 || event.keyCode === 46 || event.keyCode === 9) {
     return event.preventDefault();
-  } else {
-    event.target.value = event.target.value;
   }
-
-
+  if (event.keyCode >= 49 && event.keyCode <= 57) {
+    event.target.value = event.key;
+    userAnswer = event.key;
+  } else {
+    event.preventDefault();
+  }
 
   let numberOfRow  = ''
   let numberOfCeil = ''
+
   for (let i = 0; i < sudokuRows.length; i++) {
     if (sudokuRows[i].contains(event.target)) {
       numberOfRow = i + 1
@@ -134,6 +132,73 @@ sudokuGrid.addEventListener("keydown", function (event) {
         numberOfCeil = index + 1;
       }
     })
-    console.log(numberOfRow);
-  console.log(numberOfCeil);
+    changeGridWithEmptyCeils(gridWithEmptyCeils, numberOfRow, numberOfCeil, userAnswer);
 });
+
+function changeGridWithEmptyCeils (array, row, ceil, value) {
+  rowToNumber = Number(row)
+  ceilToNumber = Number(ceil);
+  valueString = value.toString()
+  array[rowToNumber - 1][ceilToNumber - 1] = value;
+
+    if (value >= 1 && value <= 9) {
+      moveCount++; 
+      movesOnPage.innerHTML = moveCount;
+    }
+
+  const userGridFlat = array.flat()
+  const fullGridFlat = fullGridWithAllCeils.flat()
+    if (userGridFlat[(rowToNumber - 1) * 9 + (ceilToNumber - 1)] ===
+      fullGridFlat[(rowToNumber - 1) * 9 + (ceilToNumber - 1)]) {
+      score++;
+      scoreOnPage.textContent = score;
+    }
+
+
+      let emptyCeilsCount = 0;
+      for (let i = 0; i < userGridFlat.length; i++) {
+        if (userGridFlat[i] !== fullGridFlat[i]) {
+          emptyCeilsCount++;
+        }
+      }
+
+      if (emptyCeilsCount === 0) {
+        gameEnd();
+        
+      }
+}
+
+
+console.log(fullGridWithAllCeils);
+console.log(gridWithEmptyCeils);
+
+
+function updateScore() {
+  const scoreListElement = document.querySelector('.game__score-last');
+  const scores = JSON.parse(localStorage.getItem('gameScores')) || [];
+
+  scoreListElement.innerHTML = '';
+  
+  scores.slice(0, 10).forEach((score, index) => {
+    const li = document.createElement('li');
+    li.textContent = `${index + 1}. ${score}`;
+    scoreListElement.appendChild(li);
+  });
+}
+
+function saveScoreToLS(score) {
+  let scores = JSON.parse(localStorage.getItem('gameScores')) || [];
+  scores.unshift(score);
+  if (scores.length > 10) {
+    scores = scores.slice(0, 10);
+  }
+  localStorage.setItem('gameScores', JSON.stringify(scores));
+  updateScore();
+}
+
+
+function gameEnd() {
+  console.log("You won!");
+
+  saveScoreToLS(score);
+}
